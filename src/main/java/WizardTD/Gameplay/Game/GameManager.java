1aa12e77a94faea58c0b3e5793d0f58f3e3e786e
v0x0g <v0x0g_dev@proton.me>
2023-09-16 01:25:27 +1000

    GameManager now handles levels with lines that are shorter than expected
    https://i.imgflip.com/3a8eu4.jpg

--- src/main/java/WizardTD/Gameplay/Game/GameManager.java
+++ src/main/java/WizardTD/Gameplay/Game/GameManager.java
@@ -57,11 +57,7 @@ public class GameManager {
         try {
             trace("reading layout file");
             dataStr = Files.readAllLines(path);
-            trace("layout file read: contents are \n\"\n{}\n\"",
-                    String.join("\n",
-                            (Iterable<String>) dataStr.stream().map((s) -> "'" + s + "'")::iterator
-                    )
-            );
+            trace("layout file read: contents are \n\"\n{}\n\"", String.join("\n", (Iterable<String>) dataStr.stream().map((s) -> "'" + s + "'")::iterator));
 
             return Optional.of(dataStr);
         } catch (final IOException e) {
@@ -101,27 +97,7 @@ public class GameManager {
             final double mana_pool_spell_cap_multiplier = conf.getDouble("mana_pool_spell_cap_multiplier");
             final double mana_pool_spell_mana_gained_multiplier = conf.getDouble("mana_pool_spell_mana_gained_multiplier");
 
-            gameDataConfig = new GameDataConfig(
-                    new GameDataConfig.TowerConfig(
-                            initial_tower_range,
-                            initial_tower_firing_speed,
-                            initial_tower_damage,
-                            tower_cost
-                    ),
-                    new GameDataConfig.ManaConfig(
-                            initial_mana,
-                            initial_mana_cap,
-                            initial_mana_gained_per_second
-                    ),
-                    new GameDataConfig.SpellConfig(
-                            new GameDataConfig.SpellConfig.ManaPool(
-                                    mana_pool_spell_initial_cost,
-                                    mana_pool_spell_cost_increase_per_use,
-                                    mana_pool_spell_mana_gained_multiplier,
-                                    mana_pool_spell_cap_multiplier
-                            )
-                    )
-            );
+            gameDataConfig = new GameDataConfig(new GameDataConfig.TowerConfig(initial_tower_range, initial_tower_firing_speed, initial_tower_damage, tower_cost), new GameDataConfig.ManaConfig(initial_mana, initial_mana_cap, initial_mana_gained_per_second), new GameDataConfig.SpellConfig(new GameDataConfig.SpellConfig.ManaPool(mana_pool_spell_initial_cost, mana_pool_spell_cost_increase_per_use, mana_pool_spell_mana_gained_multiplier, mana_pool_spell_cap_multiplier)));
             trace("level config={}", gameDataConfig);
         } catch (final RuntimeException e) {
             debug(e, "can't load level: failed parsing config json: (simple values)");
@@ -150,19 +126,29 @@ public class GameManager {
             }
             for (int row = 0; row < BOARD_SIZE_TILES; row++) {
                 final String line = lines.get(row);
-                if (line.length() != BOARD_SIZE_TILES) {
-                    debug("level layout invalid: line {}: expected {} chars but got {}", row + 1, BOARD_SIZE_TILES, line.length());
+                // According to https://edstem.org/au/courses/12539/discussion/1573048?comment=3524920
+                // Lines can be shorter than required, we just assume the rest is grass
+                // I still choose to fail on longer lines though
+                if (line.length() > BOARD_SIZE_TILES) {
+                    debug("level layout invalid: line {}: expected at most {} chars but got {}", row + 1, BOARD_SIZE_TILES, line.length());
                     return empty();
                 }
                 for (int col = 0; col < BOARD_SIZE_TILES; col++) {
-                    final char tileChar = line.charAt(col);
-                    final Optional<Tile> tile = Tile.fromChar(tileChar);
-                    trace("tile char [{00}] [{00}]: '{}' -> {}", row, col, tileChar, tile);
-                    if (!tile.isPresent()) {
-                        debug("invalid tile char '{}'", tileChar);
-                        return empty();
+                    if (col < line.length()) {
+                        final char tileChar = line.charAt(col);
+                        final Optional<Tile> tile = Tile.fromChar(tileChar);
+                        trace("tile char [{00}] [{00}]: '{}' -> {}", row, col, tileChar, tile);
+                        if (!tile.isPresent()) {
+                            debug("invalid tile char '{}'", tileChar);
+                            return empty();
+                        }
+                        board.setTile(row, col, tile.get());
+                    } else {
+                        // Fallback when line is shorter than expected
+                        // https://i.imgflip.com/3a8eu4.jpg
+                        trace("tile char [{00}] [{00}] fallback as grass", row, col);
+                        board.setTile(row, col, new Grass());
                     }
-                    board.setTile(row, col, tile.get());
                 }
             }
         } catch (final Exception e) {
@@ -205,11 +191,7 @@ public class GameManager {
 //        }
 
         debug("got level descriptor");
-        return Optional.of(new GameDescriptor(
-                "Test Name",
-                board,
-                gameDataConfig,
-                new ArrayList<>() //TODO: waves
+        return Optional.of(new GameDescriptor("Test Name", board, gameDataConfig, new ArrayList<>() //TODO: waves
         ));
     }
 
