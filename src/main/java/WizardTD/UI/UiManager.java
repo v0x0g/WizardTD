0e89b9cb67432830680c1d0e28fc7a8ccabc53f2
v0x0g <v0x0g_dev@proton.me>
2023-09-17 01:09:02 +1000

    Minecraft missing texture makes a comeback

--- src/main/java/WizardTD/UI/UiManager.java
+++ src/main/java/WizardTD/UI/UiManager.java
@@ -21,7 +21,7 @@ public class UiManager {
 
     private final @NonNull TaggedLogger UiLog = Logger.tag("ui");
     private final @NonNull PImage missingTextureImage = ImageExt.generatePattern(
-            GuiConfig.CELL_SIZE_PX, GuiConfig.CELL_SIZE_PX, 1, 1,
+            GuiConfig.CELL_SIZE_PX, GuiConfig.CELL_SIZE_PX, CELL_SIZE_PX >> 2, 2,
             ImageExt.ImagePattern.CHECKERS,
             Colours.BRIGHT_PURPLE.code, Colours.BLACK.code
     );
@@ -48,6 +48,8 @@ public class UiManager {
     public void renderTiles(@NonNull final PApplet app, @NonNull final GameData game) {
         // Render the game tiles
         UiLog.debug("start render tiles");
+        // Where we start rendering all tiles (global offset)
+        final int globalOffsetX = 0, globalOffsetY = TOPBAR_HEIGHT_PX;
         Vector2 tileStartCoord = new Vector2(0, TOPBAR_HEIGHT_PX);
         for (int r = 0; r < BOARD_SIZE_TILES; r++) {
             for (int c = 0; c < BOARD_SIZE_TILES; c++) {
@@ -57,11 +59,25 @@ public class UiManager {
                 UiLog.trace("tile [{00}, {00}]: tile {} img {}", r, c, tile, img);
                 if (!isValidImage(img)) {
                     img = missingTextureImage;
+                    UiLog.trace("tile [{00}, {00}]: missing texture", r, c);
                 }
-                Vector2 offset = new Vector2(r * CELL_SIZE_PX, c * CELL_SIZE_PX);
-                Vector2 pos = tileStartCoord.exactClone();
-                pos.add(offset);
-                app.image(img, (float) pos.x, (float) pos.y);
+                // Offset for this tile (relative to global offset) 
+                final int localOffsetX = r * CELL_SIZE_PX, localOffsetY = c * CELL_SIZE_PX;
+                // The start and end coordinates where we render the image (opposite corners)
+                final int startPosX = globalOffsetX + localOffsetX, startPosY = globalOffsetY + localOffsetY;
+                //  final int endPosX = globalOffsetX + localOffsetX, endPosY = globalOffsetY + localOffsetY;
+
+                // A note on how processing works:
+                // It uses a really weird filtering (I think bicubic) on images, which does look kinda wack
+                // So 
+                if (img.width != CELL_SIZE_PX || img.height != CELL_SIZE_PX) {
+                    UiLog.trace(
+                            "tile [{00}, {00}]: expected dimensions [{}x{}] but got [{}x{}], expect weird scaling", r,
+                            c, CELL_SIZE_PX, CELL_SIZE_PX, img.width, img.height
+                    );
+                    app.image(img, startPosX, startPosY, CELL_SIZE_PX, CELL_SIZE_PX);
+                }
+                else app.image(img, startPosX, startPosY);
             }
         }
     }
