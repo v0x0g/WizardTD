72071dce13c2b12cd32daf0febff66651c815858
v0x0g <v0x0g_dev@proton.me>
2023-10-17 15:34:29 +1100

    Doc comments for heap and Node

--- src/main/java/WizardTD/Gameplay/Pathfinding/Heap.java
+++ src/main/java/WizardTD/Gameplay/Pathfinding/Heap.java
@@ -4,89 +4,117 @@ import lombok.*;
 
 public class Heap {
 
-        Node[] items;
-        @Getter
-        int count;
+    /**
+     * Array of items we currently have
+     */
+    Node[] items;
+    /**
+     * How many items are currently contained in the heap
+     */
+    @Getter
+    int count;
 
-public Heap(final int maxHeapSize) {
+    public Heap(final int maxHeapSize) {
         items = new Node[maxHeapSize];
-        }
+    }
 
-public void add(final Node item) {
+    /**
+     * Adds a node to the heap
+     */
+    public void add(final Node item) {
         item.heapIndex = count;
         items[count] = item;
         SortUp(item);
         count++;
-        }
+    }
 
-public Node removeFirst() {
+    /**
+     * Removes the first node in the heap, and returns that node
+     */
+    public Node removeFirst() {
         final Node firstItem = items[0];
         count--;
         items[0] = items[count];
         items[0].heapIndex = 0;
         SortDown(items[0]);
         return firstItem;
-        }
+    }
 
-public void updateItem(final Node item) {
+    /**
+     * Updates a node's position in the heap
+     * @param item
+     */
+    public void updateItem(final Node item) {
         SortUp(item);
-        }
+    }
 
-public boolean contains(final Node item) {
+    /**
+     * Checks whether the give node is contained within this heap
+     */
+    public boolean contains(final Node item) {
         return item.equals(items[item.heapIndex]);
-        }
+    }
 
-        void SortDown(final Node item) {
+    /**
+     * Sorts a node downwards in the tree, as fasr as possible
+     */
+    void SortDown(final Node item) {
         while (true) {
-        final int childIndexLeft = item.heapIndex * 2 + 1;
-        final int childIndexRight = item.heapIndex * 2 + 2;
-        int swapIndex;
+            final int childIndexLeft = item.heapIndex * 2 + 1;
+            final int childIndexRight = item.heapIndex * 2 + 2;
+            int swapIndex;
 
-        if (childIndexLeft < count) {
-        swapIndex = childIndexLeft;
+            if (childIndexLeft < count) {
+                swapIndex = childIndexLeft;
 
-        if (childIndexRight < count) {
-        if (items[childIndexLeft].compareTo(items[childIndexRight]) < 0) {
-        swapIndex = childIndexRight;
-        }
-        }
+                if (childIndexRight < count) {
+                    if (items[childIndexLeft].compareTo(items[childIndexRight]) < 0) {
+                        swapIndex = childIndexRight;
+                    }
+                }
 
-        if (item.compareTo(items[swapIndex]) < 0) {
-        Swap (item,items[swapIndex]);
-        }
-        else {
-        return;
-        }
+                if (item.compareTo(items[swapIndex]) < 0) {
+                    Swap(item, items[swapIndex]);
+                }
+                else {
+                    return;
+                }
+
+            }
+            else {
+                return;
+            }
 
         }
-        else {
-        return;
-        }
+    }
 
-        }
-        }
-
-        void SortUp(final Node item) {
-        int parentIndex = (item.heapIndex-1)/2;
+    /**
+     * Sorts a node downwards in the tree, as fasr as possible
+     */
+    void SortUp(final Node item) {
+        int parentIndex = (item.heapIndex - 1) / 2;
 
         while (true) {
-        final Node parentItem = items[parentIndex];
-        if (item.compareTo(parentItem) > 0) {
-        Swap (item,parentItem);
-        }
-        else {
-        break;
-        }
+            final Node parentItem = items[parentIndex];
+            if (item.compareTo(parentItem) > 0) {
+                Swap(item, parentItem);
+            }
+            else {
+                break;
+            }
 
-        parentIndex = (item.heapIndex-1)/2;
+            parentIndex = (item.heapIndex - 1) / 2;
         }
-        }
+    }
 
-        void Swap(final Node itemA, final Node itemB) {
+    /**
+     * Swaps two nodes in the heap
+     */
+    void Swap(final Node itemA, final Node itemB) {
         items[itemA.heapIndex] = itemB;
         items[itemB.heapIndex] = itemA;
         final int itemAIndex = itemA.heapIndex;
         itemA.heapIndex = itemB.heapIndex;
         itemB.heapIndex = itemAIndex;
-        }
-        }
+    }
+}
--- src/main/java/WizardTD/Gameplay/Pathfinding/Node.java
+++ src/main/java/WizardTD/Gameplay/Pathfinding/Node.java
@@ -6,15 +6,34 @@ import lombok.*;
 @ToString
 @EqualsAndHashCode
 public class Node implements Comparable<Node> {
+    /**
+     * Is this tile a valid path that can be walked upon (traversed)
+     */
     public boolean walkable;
+    /**
+     * Position of this node in Tiles
+     */
     public TilePos pos;
+    /**
+     * Parent node of this node (since we have a heap/tree structure)
+     */
     public Node parent;
+    /**
+     * Numeric index of this node in the heap
+     */
     public int heapIndex;
-    @Getter
-    double gCost;
-    @Getter
-    double hCost;
 
+    /**
+     * See an A* tutorial for what this is
+     */
+    @Getter
+    double gCost, hCost;
+
+    /**
+     * See an A* tutorial for what this is
+     *
+     * @return
+     */
     public double getFCost() {
         return this.gCost + this.hCost;
     }
