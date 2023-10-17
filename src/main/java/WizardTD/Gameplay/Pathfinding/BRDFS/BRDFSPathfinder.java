59be42f9d5aa680053100f92467b5636fd40e749
v0x0g <v0x0g_dev@proton.me>
2023-10-17 22:13:13 +1100

    Gave up

--- src/main/java/WizardTD/Gameplay/Pathfinding/BRDFS/BRDFSPathfinder.java
+++ src/main/java/WizardTD/Gameplay/Pathfinding/BRDFS/BRDFSPathfinder.java
@@ -9,6 +9,7 @@ import org.checkerframework.checker.nullness.qual.*;
 
 import java.util.*;
 
+import static WizardTD.GameConfig.BOARD_SIZE_TILES;
 import static java.lang.Math.*;
 
 @UtilityClass
@@ -32,49 +33,41 @@ public class BRDFSPathfinder {
     }
 
     public @Nullable List<EnemyPath> findPaths(final Board board, final TilePos startPos, final TilePos endPos) {
-        /*
-        * ===== DEPTH FIRST SEARCH =====
-        */
-        final Vertex finalNode;
-        
         final Queue<Vertex> queue = new ArrayDeque<>();
         final Set<Vertex> explored = new HashSet<>();
         final Vertex root = new Vertex(board.getTile(startPos.getX(), startPos.getY()), null);
         queue.add(root);
-        
-        while (!queue.isEmpty()) {
+
+        final List<EnemyPath> solutions = new ArrayList<>();
+        while ((!queue.isEmpty()) && (explored.size() < BOARD_SIZE_TILES * BOARD_SIZE_TILES)) {
             final Vertex v = queue.remove();
             // If we have found the target end node
             if (v.tile.getPos().equals(endPos)) {
-                finalNode = v;
-                break;
-//                // Build up the path by going 
-//                final Stack<TilePos> path = new Stack<>();
-//                Vertex x = v;
-//                while (x.parent != null) {
-//                    path.push(x.tile.getPos());
-//                    x = x.parent;
-//                }
-//                solutions.add(
-//                        // Why the hell do I need to allocate a zero-sized array
-//                        // Just so that the type is recognised
-//                        // What the **** is wrong with this language
-//                        new EnemyPath(path.toArray(new TilePos[0]))
-//                );
+                // Build up the path by going 
+                final Stack<TilePos> path = new Stack<>();
+                Vertex x = v;
+                while (x.parent != null) {
+                    path.push(x.tile.getPos());
+                    x = x.parent;
+                }
+                solutions.add(
+                        // Why the hell do I need to allocate a zero-sized array
+                        // Just so that the type is recognised
+                        // What the **** is wrong with this language
+                        new EnemyPath(path.toArray(new TilePos[0]))
+                );
             }
             adjacentEdges(board, v)
                     .forEach(w -> {
-                        if (!explored.contains(w)) {
-                            explored.add(w);
-                            w.parent = v;
-                            queue.add(w);
-                        }
+//                        if (!explored.contains(w)) {
+                        explored.add(w);
+                        w.parent = v;
+                        queue.add(w);
+//                        }
                     });
             explored.add(v);
         }
-        
-        
-        return ;
+        return solutions;
     }
 
     /**
