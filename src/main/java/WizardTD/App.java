f1970ab9d25dbbe34f74dcbed9b792d507d53a55
v0x0g <v0x0g_dev@proton.me>
2023-10-20 16:57:58 +1100

    Stupid OP pathfinding shit

--- src/main/java/WizardTD/App.java
+++ src/main/java/WizardTD/App.java
@@ -154,7 +154,7 @@ public final class App extends PApplet {
         final double deltaTime = thisTick - lastTick;
         this.lastFrameTime = thisTick;
 
-        Loggers.RENDER.trace("background");
+         Loggers.RENDER.trace("background");
         background(Colour.DEEP_PURPLE.asInt());
 
         GameManager.tickGameWithSubtick(this, this.gameData, deltaTime);
@@ -193,7 +193,7 @@ public final class App extends PApplet {
             this.text(str, x1, y1);
 
 
-            Debug.drawPathfindingOverlay(this, gameData);
+//            Debug.drawPathfindingOverlay(this, gameData);
             Debug.drawHoveredTileOverlay(this, gameData);
         }
 
