d2b1f6d1816dcc5d5eb298c1616b945cfe3d0031
v0x0g <v0x0g_dev@proton.me>
2023-10-27 21:54:26 +1100

    Finally

--- src/test/java/WizardTD/Gameplay/Integration/GameRunsTests.java
+++ src/test/java/WizardTD/Gameplay/Integration/GameRunsTests.java
@@ -4,22 +4,16 @@ import WizardTD.*;
 import org.junit.jupiter.api.*;
 import processing.core.*;
 
+import static java.lang.Thread.sleep;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GameRunsTests extends IntegrationTest {
     @Test
-    public void gameRuns() throws InterruptedException, App.AppInitException {
-//        System.setProperty("java.awt.headless", "true");
-//        App app = assertDoesNotThrow(IntegrationTestApp::new);
-//        app.noLoop();
-//        App.runSketch(new String[]{}, app);
-//        Thread.sleep(10000);
-//        app.exit();
-
-        App app = new App();
-        app.loop();
-        PApplet.runSketch(new String[] { "App" }, app);
-        app.setup();
-//        app.delay(5000);
+    public void gameRuns() throws InterruptedException {
+        sleep(1000);
+        assertFalse(app.finished);
+        app.exit();
+        sleep(1000);
+        assertTrue(app.finished);
     }
 }
--- src/test/java/WizardTD/Gameplay/Integration/IntegrationTest.java
+++ src/test/java/WizardTD/Gameplay/Integration/IntegrationTest.java
@@ -1,10 +1,21 @@
 package WizardTD.Gameplay.Integration;
 
+import WizardTD.*;
+import lombok.*;
+import org.checkerframework.checker.nullness.qual.NonNull;
 import org.junit.jupiter.api.*;
+import processing.core.*;
 
 public abstract class IntegrationTest {
+    @SuppressWarnings("DataFlowIssue")
+    @NonNull App app = null;
+    
+    @SneakyThrows
     @BeforeEach
-    public void setUpHeadlessMode() {
-//        System.setProperty("java.awt.headless", "true");
+    public void setUpApp() {
+        app = new IntegrationTestApp();
+        app.loop();
+        PApplet.runSketch(new String[] { "App" }, app);
+        app.setup();
     }
 }
